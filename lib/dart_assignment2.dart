0deac47aaa4718acbcb1f1f10dabff25a6fe3666
지은성 <ddrr2852@gmail.com>
2025-12-04 19:32:46 +0900

    두번째문제 완성

--- lib/dart_assignment2.dart
+++ lib/dart_assignment2.dart
@@ -1,5 +1,27 @@
-// 가상의 쇼핑몰에서 결제 금액을 계산하는 프로그램을 만들어봅니다.
-// 
-// 1. 구매자가 장바구니에 담은 상품들의 전체 금액을 계산합니다.
-// 2. 총 금액이 20,000원 이상일 경우, 10% 할인을 적용합니다.
-// 3. 최종적으로 구매자가 결제해야 하는 금액을 출력합니다.
+void main() {
+  Map<String, int> price = {'티셔츠': 10000, '바지': 8000, '모자': 4000};
+
+  List<String> cart = ["티셔츠", "바지", "모자", "티셔츠", "바지"];
+
+  int total = 0;
+
+  for (var item in cart) {
+    // print(item);
+    // print(price[item]);
+    total += price[item]!;
+    // 토탈 + 아이템이 담긴 프라이스 = 토탈
+  }
+  print("장바구니에 $total원 어치를 담으셨네요!");
+  // 상품 총 가격 구함
+
+  double dc = 0.1;
+  if (total >= 20000) {
+    // 구매 금액의 10%
+    dc *= total;
+    // 10% 출력
+
+    print("할인금액 : ${dc.toInt()}원");
+    double realTotal = total - dc;
+    print("최종적으로 결제하실 금액은 : ${realTotal.toInt()}원 입니다!");
+  }
+}
