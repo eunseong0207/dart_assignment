7e0b33e64c9adc5055809871454863d36d2c4b8d
지은성 <ddrr2852@gmail.com>
2025-12-04 11:47:37 +0900

    첫번째 풀이

--- bin/dart_assignment.dart
+++ bin/dart_assignment.dart
@@ -1,5 +1,1 @@
-import 'package:dart_assignment/dart_assignment.dart' as dart_assignment;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_assignment.calculate()}!');
-}
+void main() {}
